07d58fe97118e6dff529366e3781df291b454b34
Denis Eroshevskii <devskjj@gmail.com>
2026-02-07 12:52:07 +0600

    "Загрузил проект, добавил необходимые зависимости."

---
+++ src/main/java/kg/attractor/filehub/FileHubApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.filehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileHubApplication.class, args);
+    }
+
+}
